5ce0159b89870e7f73396643aedd9bef6cf643d2
mattkia <matkia1992@gmail.com>
2026-05-24 14:42:20 -0600

    Update the layout

--- static/js/index.js
+++ static/js/index.js
@@ -1,12 +1,12 @@
 window.HELP_IMPROVE_VIDEOJS = false;
 
-var INTERP_BASE = "./static/interpolation/stacked";
-var NUM_INTERP_FRAMES = 240;
+var INTERP_BASE = "./static/images/oasis";
+var NUM_INTERP_FRAMES = 100;
 
 var interp_images = [];
 function preloadInterpolationImages() {
   for (var i = 0; i < NUM_INTERP_FRAMES; i++) {
-    var path = INTERP_BASE + '/' + String(i).padStart(6, '0') + '.jpg';
+    var path = INTERP_BASE + '/cframe_' + String(i) + '.png';
     interp_images[i] = new Image();
     interp_images[i].src = path;
   }
